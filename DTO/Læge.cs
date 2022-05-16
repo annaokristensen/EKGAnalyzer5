673e992faebabe8687335e175f4463549b4b4aaf
Anna Kristensen <annaokristensen@gmail.com>
2022-05-16 12:08:58 +0200

    SendToDatabase opdateret

---
+++ DTO/Læge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Læge
+    {
+        public string Organisation { get; set; }
+        public string ID { get; set; }
+
+        public Læge(string organisation, string id)
+        {
+            Organisation = organisation;
+            ID = id;
+        }
+    }
+}
--- DataLayer/OffentligDatabase.cs
+++ DataLayer/OffentligDatabase.cs
@@ -28,7 +28,7 @@ namespace DataLayer
                 return con;
             }
         }
-        public void SendToDatabase(EKG ekg)
+        public void SendToDatabase(EKG ekg, Læge læge)
         {
 
             string insertStringParam = @"INSERT INTO EKGDATA (raa_data, ekgmaaleid, samplerate_hz, interval_sec, data_format, bin_eller_tekst, maaleformat_type, start_tid) OUTPUT INSERTED.ekgdataid VALUES(@data, 164, 400, 3600, N'2015-04-27', '1', N'double', CONVERT(DATETIME, @starttid, 102))"; //Datetime, 102???
@@ -42,7 +42,22 @@ namespace DataLayer
                 cmd.Parameters.AddWithValue("@starttid", starttid); //ER DET HER RIGTIGT??!!
                 // Get your parameters ready
                 cmd.Parameters.AddWithValue("@data", data.ToArray().SelectMany(value => BitConverter.GetBytes(value)).ToArray());
-                long id = (long)cmd.ExecuteScalar(); //Returns the identity of the new tuple/record 64 bit/8 bytes
+                long id = (long)cmd.ExecuteScalar(); //Returns the identity of the new tuple/record 64 bit/8 bytes //HVad gør det her?
+            }
+
+            string insertStringParam2 =
+                @"INSERT INTO EKGMAELING (ekgmaaleid, dato, antalmaalinger, sfp_ansvrmedarbjnr, sfp_ans_org, borger_cprnr) OUTPUT INSERTED.ekgmaaleid VALUES(164, @dato, @Id, @org, @cpr)";
+            DateTime dato = ekg.MeasurementTime;
+            string cpr = ekg.CPR;
+            string org = læge.Organisation;
+            string Id = læge.ID;
+
+            using (SqlCommand cmd = new SqlCommand(insertStringParam2, OpenConnectionST))
+            {
+                cmd.Parameters.AddWithValue("@dato", dato);
+                cmd.Parameters.AddWithValue("@org", org);
+                cmd.Parameters.AddWithValue("@cpr", cpr);
+                cmd.Parameters.AddWithValue("@Id", Id);
             }
         }
 
