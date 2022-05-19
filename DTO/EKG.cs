6719de653826f3fc29ee6385a45722bbf6feeeb3
Anna Kristensen <annaokristensen@gmail.com>
2022-05-19 09:35:58 +0200

    LokalDatabase oprettet

--- DTO/EKG.cs
+++ DTO/EKG.cs
@@ -6,15 +6,25 @@ namespace DTO
     public class EKG
     {
         public List<double> EKGsamples { get; set; }
-        public double MeasurementPeriod { get; set; }
+        public double SampleRate { get; set; }
+        public int IntervalSec { get; set; }
+        public string DataFormat { get; set; }
+        public string BinEllerTekst { get; set; }
+        public string Maaleformat_type { get; set; }
         public string CPR { get; set; }
-
         public DateTime MeasurementTime{ get; set; }
 
-        public EKG(List<double> ekgSamples, double measurementPeriod, string cpr, DateTime measurementTime)
+        public EKG()
+        {}
+
+        public EKG(List<double> ekgSamples, double sampleRate, int intervalSec,string dataFormat, string binEllerTekst, string maaleformatType, string cpr, DateTime measurementTime)
         {
             EKGsamples = ekgSamples;
-            MeasurementPeriod = measurementPeriod;
+            SampleRate = sampleRate;
+            IntervalSec = intervalSec;
+            DataFormat = dataFormat;
+            BinEllerTekst = binEllerTekst;
+            Maaleformat_type = maaleformatType;
             CPR = cpr;
             MeasurementTime = measurementTime;
         }
--- DataLayer/LokalDatabase.cs
+++ DataLayer/LokalDatabase.cs
@@ -4,14 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DTO;
+using System.Data.SqlClient;
 
 namespace DataLayer
 {
     public class LokalDatabase
     {
-        public EKG GetEKG()
+        private SqlConnection connection;
+        private SqlCommand command;
+        private const string db = "EKG_Lokal";
+        public LokalDatabase()
         {
-            return new EKG(new List<double>(), 12, "1111111", DateTime.Now);
+            
+        }
+        private SqlConnection OpenConnectionST
+        {
+            get
+            {
+                var con = new SqlConnection(@"Data Source=BBLAP18\SQLEXPRESS;Initial Catalog=EKG_Lokal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                con.Open();
+                return con;
+            }
+        }
+        public EKG GetEKG(string CPR)
+        {
+            EKG ekg = new EKG();
+
+            //CPR
+            ekg.CPR = CPR;
+
+
+            //BLOB laves til liste
+            byte[] bytesArr = new byte[800];
+            List<double> list = new List<double>();
+            SqlDataReader rdr;
+            string selectString = "Select * from dbo.EKGLokal where cpr_borger = " + CPR;
+            using (SqlCommand cmd = new SqlCommand(selectString, connection))
+            {
+                rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                    bytesArr = (byte[])rdr["raa_data"];
+                for (int i = 0, j = 0; i < bytesArr.Length; i += 8, j++)
+                    list.Add(BitConverter.ToDouble(bytesArr, i)); 
+            }
+
+            ekg.EKGsamples = list;
+
+            //Resten af informationerne udtrækkes
+            using (SqlCommand cmd = new SqlCommand(selectString, connection))
+            {
+                rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    ekg.BinEllerTekst = Convert.ToString(rdr["bin_eller_tekst"].ToString());
+                    ekg.IntervalSec = Convert.ToInt32((int)rdr["interval_sec"]);
+                    ekg.SampleRate = Convert.ToDouble((float)rdr["samplerate_hz"]);
+                    ekg.DataFormat = Convert.ToString(rdr["data_format"].ToString());
+                    ekg.Maaleformat_type = Convert.ToString(rdr["maaleformat_type"].ToString());
+                    ekg.MeasurementTime = Convert.ToDateTime((DateTime)rdr["start_tid"]);
+                }
+
+            }
+            return ekg;
         }
 
         public bool isUserRegistered(string cpr)
