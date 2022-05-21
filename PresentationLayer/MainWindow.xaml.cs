fad10880841a6741528466db3ae1003c0dc6ac30
Anna Kristensen <annaokristensen@gmail.com>
2022-05-21 09:25:02 +0200

    DataContext = this tilføjet (Anna og Rike)

--- PresentationLayer/MainWindow.xaml.cs
+++ PresentationLayer/MainWindow.xaml.cs
@@ -61,6 +61,8 @@ namespace PresentationLayer
             TBLaegehus2.Text = laegehus;
 
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            DataContext = this;
+
         }
 
         public string Cpr
